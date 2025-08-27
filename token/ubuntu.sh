f5f9af57840c53203b23dfc6d64ad37729810608
theDreamer911 <thetrulydreamer@gmail.com>
2025-08-27 14:51:35 +0700

    renew agent august 25

--- token/ubuntu.sh
+++ token/ubuntu.sh
@@ -5,11 +5,11 @@ mkdir -p sent1
 cd sent1
 
 # Download Sentinel Agent and SiteToken
-wget https://github.com/samphoerna/Sentinel-Repo/raw/main/Linux/SentinelAgent_linux_x86_64_v24_2_1_8.deb
+wget https://github.com/theDreamer911/repo-sentinel/raw/refs/heads/main/ubuntu/agent_linux_v25.deb
 wget https://raw.githubusercontent.com/theDreamer911/repo-sentinel/refs/heads/main/token/sentinelToken269.txt
 
 # Install Sentinel Agent
-sudo dpkg -i SentinelAgent_linux_x86_64_v24_2_1_8.deb
+sudo dpkg -i agent_linux_v25.deb
 
 # Set the management token
 /opt/sentinelone/bin/sentinelctl management token set $(cat SiteToken.txt)
