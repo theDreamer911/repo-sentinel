f2683cec343ecc3017620f719b60741494f60c1d
theDreamer911 <thetrulydreamer@gmail.com>
2025-08-28 11:02:40 +0700

    updated 2808

--- token/ubuntu.sh
+++ token/ubuntu.sh
@@ -5,14 +5,14 @@ mkdir -p sent1
 cd sent1
 
 # Download Sentinel Agent and SiteToken
-wget https://github.com/theDreamer911/repo-sentinel/raw/refs/heads/main/ubuntu/agent_linux_v25.deb
+wget https://github.com/theDreamer911/repo-sentinel/raw/refs/heads/main/ubuntu/amd_linux_agent.deb
 wget https://raw.githubusercontent.com/theDreamer911/repo-sentinel/refs/heads/main/token/sentinelToken269.txt
 
 # Install Sentinel Agent
-sudo dpkg -i agent_linux_v25.deb
+sudo dpkg -i amd_linux_agent.deb
 
 # Set the management token
-/opt/sentinelone/bin/sentinelctl management token set $(cat SiteToken.txt)
+/opt/sentinelone/bin/sentinelctl management token set $(cat sentinelToken269.txt)
 
 # Start the Sentinel agent
 /opt/sentinelone/bin/sentinelctl control start
